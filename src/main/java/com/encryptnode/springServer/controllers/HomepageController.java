f37432dd5267c372cd334daf78e1465038b08101
Tyler Pearson <tylerthewonderfull@gmail.com>
2018-08-08 09:12:25 -0700

    ugh

--- src/main/java/com/encryptnode/springServer/controllers/HomepageController.java
+++ src/main/java/com/encryptnode/springServer/controllers/HomepageController.java
@@ -7,14 +7,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class HomepageController {
-
-
-    @GetMapping("/")
-    public String homePage(Model model) {
-
-        System.out.println("Hit Home controller");
-        return "index";
-    }
-}
+//@Controller
+//public class HomepageController {
+//
+//
+//    @GetMapping("/")
+//    public String homePage(Model model) {
+//
+//        System.out.println("Hit Home controller");
+//        return "index";
+//    }
+//}
